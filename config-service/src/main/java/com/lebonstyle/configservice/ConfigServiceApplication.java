1bf76cb999b304bdc702f35126ac70f232e35f4f
hamdi-ali-git <hamdifreelance365@gmail.com>
2024-11-16 13:00:13 +0100

    configure config repository

--- config-service/src/main/java/com/lebonstyle/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/lebonstyle/configservice/ConfigServiceApplication.java
@@ -2,8 +2,9 @@ package com.lebonstyle.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableServerConfig
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
 
